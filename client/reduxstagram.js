82f47c9a339922fa5c6fa2d7d84eaba424d8d05c
paul clatworthy <strayllama@gmail.com>
2018-08-03 11:01:23 +0100

    inital comit

---
+++ client/reduxstagram.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+render(<p>hello</p>, document.getElementById('root'));
+
